ee6e6acdbd059e7c23d4ad51ba5b2d5ebe9ff777
Manoj-Kumar-Munda <manojkrmunda828@gmail.com>
2026-02-08 22:41:30 +0530

    feat: add environment variable support and update default port in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
@@ -12,7 +12,7 @@ app.get("/products", (req, res) => {
   res.send("Get all products");
 });
 app.get("/", (req, res) => {
-  res.send("Hello World v3!");
+  res.send(`Hello World v3! SECRET: ${process.env.SECRET}`);
 });
 
 app.listen(port, () => {
